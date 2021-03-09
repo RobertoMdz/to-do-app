b03de24a86902de44515a8c300e6039e2e411527
RobertoMdz <roberto.cr.mendoza@gmail.com>
2021-03-08 22:04:46 -0500

    Initials features of to do app project

---
+++ app/src/main/java/com/coopera/todoappproject/fragments/list/ListFragment.kt
@@ -0,0 +1,35 @@
+package com.coopera.todoappproject.fragments.list
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.coopera.todoappproject.R
+import com.coopera.todoappproject.databinding.FragmentListBinding
+
+class ListFragment : Fragment() {
+
+    private var _binding: FragmentListBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentListBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        binding.floatingActionButton.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_addFragment)
+        }
+
+        binding.listLayout.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_updateFragment)
+        }
+        return view
+    }
+
+}
