317279c283704483c32434311be5217b13d5b109
RobertoMdz <roberto.cr.mendoza@gmail.com>
2021-03-13 13:12:34 -0500

    to do list menu created

--- app/src/main/java/com/coopera/todoappproject/fragments/list/ListFragment.kt
+++ app/src/main/java/com/coopera/todoappproject/fragments/list/ListFragment.kt
@@ -1,10 +1,8 @@
 package com.coopera.todoappproject.fragments.list
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.coopera.todoappproject.R
 import com.coopera.todoappproject.databinding.FragmentListBinding
@@ -29,7 +27,16 @@ class ListFragment : Fragment() {
         binding.listLayout.setOnClickListener {
             findNavController().navigate(R.id.action_listFragment_to_updateFragment)
         }
+
+        // Set menu
+        setHasOptionsMenu(true)
         return view
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+       inflater.inflate(R.menu.list_fragment_menu, menu)
+    }
+
+
 
 }
