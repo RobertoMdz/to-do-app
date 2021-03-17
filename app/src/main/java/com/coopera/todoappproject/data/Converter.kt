dd19d35597184224f35c84de74101a42cce37900
RobertoMdz <roberto.cr.mendoza@gmail.com>
2021-03-16 23:48:30 -0500

    types Converter implemented

--- app/src/main/java/com/coopera/todoappproject/data/Converter.kt
+++ app/src/main/java/com/coopera/todoappproject/data/Converter.kt
@@ -1,5 +1,17 @@
 package com.coopera.todoappproject.data
 
+import androidx.room.TypeConverter
+
 class Converter {
+
+    @TypeConverter
+    fun fromPriority(priority: Priority): String {
+        return priority.name
+    }
+
+    @TypeConverter
+    fun toPriority(priority: String ): Priority {
+        return Priority.valueOf(priority)
+    }
 
 }
