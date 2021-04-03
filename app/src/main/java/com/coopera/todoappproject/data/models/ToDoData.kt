6e6a99610f4e421d3727b687ca47753520f7e594
RobertoMdz <roberto.cr.mendoza@gmail.com>
2021-04-03 00:12:18 -0500

    change parcelize plugin to kotlinx.android.parcel

--- app/src/main/java/com/coopera/todoappproject/data/models/ToDoData.kt
+++ app/src/main/java/com/coopera/todoappproject/data/models/ToDoData.kt
@@ -3,8 +3,8 @@ package com.coopera.todoappproject.data.models
 import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.coopera.todoappproject.data.models.Priority
-import kotlinx.parcelize.Parcelize
+import kotlinx.android.parcel.Parcelize
+
 
 @Entity(tableName = "todo_table")
 @Parcelize
