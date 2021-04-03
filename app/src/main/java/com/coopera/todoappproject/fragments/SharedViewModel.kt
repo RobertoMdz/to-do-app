8a26f7c6ab18e53b720b1528fefd12ef92ec31fa
RobertoMdz <roberto.cr.mendoza@gmail.com>
2021-04-03 00:00:55 -0500

    refactor if condition in shared view model

--- app/src/main/java/com/coopera/todoappproject/fragments/SharedViewModel.kt
+++ app/src/main/java/com/coopera/todoappproject/fragments/SharedViewModel.kt
@@ -44,9 +44,7 @@ class SharedViewModel(application: Application): AndroidViewModel(application) {
     }
 
     fun verifyDataFromUser(title: String, description: String): Boolean {
-        return if(TextUtils.isEmpty(title) || TextUtils.isEmpty(description)) {
-            false
-        } else !(title.isEmpty() || description.isEmpty())
+        return !(title.isEmpty() || description.isEmpty())
     }
 
     /** ====================== List Fragment =========================== **/
